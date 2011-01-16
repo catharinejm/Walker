06cedc89baddf24ac7671a8d5e4410bacf3ba1e6
Sam Umbach and Jon Distad <pair@thinkrelevance.com>
2011-01-16 00:43:43 -0500

    moving sprite

--- sam.rb
+++ sam.rb
@@ -15,6 +15,8 @@ class Sam < Gosu::Window
     super SWIDTH, SHEIGHT, false
     @x = 0
     @z = ZMIN
+
+    @images = Gosu::Image.load_tiles(self, "player.png", -2, -4, false)
   end
 
   def needs_cursor?() true end
@@ -32,6 +34,8 @@ class Sam < Gosu::Window
     when Gosu::KbDown
       @down = true
     end
+    @start_moving = Time.now
+    @step = 0
   end
 
   def button_up key
@@ -58,6 +62,11 @@ class Sam < Gosu::Window
     @x = -width/2.0 if @x < -width/2.0
     @x = width/2.0 if @x > width/2.0
 
+    if Time.now.to_f - @start_moving.to_f > 0.2
+      @step = @step.to_i + 1 
+      @start_moving = Time.now
+    end
+
     puts "x: #@x, z: #@z"
   end
 
@@ -69,18 +78,32 @@ class Sam < Gosu::Window
     ((height/2.0-y) * height/2.0/(z*Math.tan(FOV_Y/2.0)) + height/2.0)/RATIO
   end
 
+  def image
+    return @images[6+@step%2] if @left
+    return @images[2+@step%2] if @up
+    return @images[4+@step%2] if @right
+    @images[0+@step%2]
+  end
+
   def draw
     c = Gosu::Color::RED
     b = Gosu::Color::BLUE
+    clear = Gosu::Color::WHITE
 
     draw_quad(
       screen_x(-512, ZMIN), screen_y(0, ZMIN), b,
       screen_x(512, ZMIN), screen_y(0, ZMIN), b,
       screen_x(-512, ZMAX), screen_y(0, ZMAX), b,
       screen_x(512, ZMAX), screen_y(0, ZMAX), b)
-    draw_triangle(
-      screen_x(@x-CHAR_HT/2, @z), screen_y(0, @z), c,
-      screen_x(@x+CHAR_HT/2, @z), screen_y(0, @z), c,
-      screen_x(@x, @z), screen_y(CHAR_HT, @z), c)
+
+    image.draw_as_quad(
+      screen_x(@x-CHAR_HT/2, @z), screen_y(CHAR_HT, @z), clear,
+      screen_x(@x+CHAR_HT/2, @z), screen_y(CHAR_HT, @z), clear,
+      screen_x(@x-CHAR_HT/2, @z), screen_y(0, @z), clear,
+      screen_x(@x+CHAR_HT/2, @z), screen_y(0, @z), clear, @z)
+    # draw_triangle(
+    #   screen_x(@x-CHAR_HT/2, @z), screen_y(0, @z), c,
+    #   screen_x(@x+CHAR_HT/2, @z), screen_y(0, @z), c,
+    #   screen_x(@x, @z), screen_y(CHAR_HT, @z), c)
   end
 end
