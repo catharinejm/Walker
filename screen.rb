582ff4eade1f662db8d5fe2d4d70127be2cdeac5
Jon Distad <jon.distad@gmail.com>
2011-01-23 23:58:43 -0500

    use constants for coord calculations

--- screen.rb
+++ screen.rb
@@ -7,18 +7,18 @@ module Screen
   ZMIN = SWIDTH/2.0/Math.tan(FOV_X/2.0)
 
   def screen_x wx, wz
-    wx*ZMIN/wz + width/2.0
+    wx*ZMIN/wz + SWIDTH/2.0
   end
 
   def screen_y wy, wz
-    height/2.0 - ((wy-height/2.0)*ZMIN/wz)
+    SHEIGHT/2.0 - ((wy-SHEIGHT/2.0)*ZMIN/wz)
   end
 
   def world_x sx, sy, wy=0
-    (sx-width/2.0)*(wy-height/2.0) / (height/2.0 - sy)
+    (sx-SWIDTH/2.0)*(wy-SHEIGHT/2.0) / (SHEIGHT/2.0 - sy)
   end
 
   def world_z sy, wy=0
-    (wy-height/2.0)*ZMIN / (height/2.0 - sy)
+    (wy-SHEIGHT/2.0)*ZMIN / (SHEIGHT/2.0 - sy)
   end
 end
